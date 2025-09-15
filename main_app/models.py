5db22a3f26cfa40d5625169aaaf3bc85c4c4cbf8
devin <devinm3333@outlook.com>
2025-09-15 12:11:03 -0700

    initial

---
+++ main_app/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class PurchaseLocation(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    image = models.TextField(max_length=255, blank=True)
+
+
+class Item(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    image = models.TextField(max_length=255, blank=True)
+    purchase_locations = models.ManyToManyField(PurchaseLocation)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ingredient-detail", kwargs={"pk": self.id})
+
+
+class Pantry(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(
+        User,
+        related_name='pantry',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+    def all_items(self):
+        return self.items.select_related("item")
+
+
+class PantryItem(models.Model):
+    pantry = models.ForeignKey(Pantry, related_name='items')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        unique_together = ("pantry", "item")
+
+    def __str__(self):
+        return f"{self.item.name}"
---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
